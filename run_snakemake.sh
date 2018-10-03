5ba9b6988a3fd244548e08d3abfcd53a825c921c
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2018-10-03 11:22:21 +0200

    snakemake dev

---
+++ run_snakemake.sh
@@ -0,0 +1,2 @@
+
+snakemake -np mapped_reads/{A,B}.bam
---
+++ snakemakegrene.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+Pipeline workflow for grenepool
+
+@author: Moises Exposito-Alonso (moisesexpositoalonso@gmail.com)
+
+'''
+
+
